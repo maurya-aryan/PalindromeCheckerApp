a8bee29e3fba428bbf0f9152b36ff2ac409c53b0
maurya-aryan <aryan5592ar@gmail.com>
2026-02-26 16:25:18 +0530

    UC4: Added

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,4 +1,4 @@
-public class PalindromeChecker  {
+public class PalindromeChecker {
 
     public static void main(String[] args) {
 
@@ -10,6 +10,9 @@ public class PalindromeChecker  {
 
         // UC3
         checkPalindromeUsingLoop();
+
+        // UC4
+        checkPalindromeUsingCharArray();
     }
 
     // =========================
@@ -21,7 +24,7 @@ public class PalindromeChecker  {
     }
 
     // =========================
-    // UC2 - Hardcoded Palindrome (Using StringBuilder)
+    // UC2 - Hardcoded Palindrome (StringBuilder)
     // =========================
     public static void checkHardcodedPalindrome() {
 
@@ -29,14 +32,14 @@ public class PalindromeChecker  {
         String reversed = new StringBuilder(word).reverse().toString();
 
         if (word.equals(reversed)) {
-            System.out.println("UC2 Result: " + word + " is a Palindrome");
+            System.out.println("UC2: " + word + " is a Palindrome");
         } else {
-            System.out.println("UC2 Result: " + word + " is NOT a Palindrome");
+            System.out.println("UC2: " + word + " is NOT a Palindrome");
         }
     }
 
     // =========================
-    // UC3 - Palindrome Using Loop Reverse
+    // UC3 - Reverse Using Loop
     // =========================
     public static void checkPalindromeUsingLoop() {
 
@@ -48,9 +51,40 @@ public class PalindromeChecker  {
         }
 
         if (original.equals(reversed)) {
-            System.out.println("UC3 Result: " + original + " is a Palindrome");
+            System.out.println("UC3: " + original + " is a Palindrome");
         } else {
-            System.out.println("UC3 Result: " + original + " is NOT a Palindrome");
+            System.out.println("UC3: " + original + " is NOT a Palindrome");
+        }
+    }
+
+    // =========================
+    // UC4 - Character Array + Two Pointer
+    // =========================
+    public static void checkPalindromeUsingCharArray() {
+
+        String input = "level";
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("UC4: " + input + " is a Palindrome");
+        } else {
+            System.out.println("UC4: " + input + " is NOT a Palindrome");
         }
     }
 }
